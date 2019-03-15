593c63c54a0c652197d9f5d4195535d48587aae7
cockroach <weekend8394@gmail.com>
2019-03-15 14:21:28 +0800

    3/15

---
+++ FootPrintIOS/NewsTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  NewsTableViewCell.swift
+//  FootPrintIOS
+//
+//  Created by Cockroach on 2019/3/14.
+//  Copyright © 2019 lulu. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    @IBOutlet weak var bt_HeadPicture: UIButton!
+    @IBOutlet weak var bt_NickName: UIButton!
+    @IBOutlet weak var bt_LandMark: UIButton!
+    @IBOutlet weak var iv_NewsPicture: UIImageView!
+    @IBOutlet weak var bt_Like: UIButton!
+    @IBOutlet weak var bt_Message: UIButton!
+    @IBOutlet weak var bt_Collection: UIButton!
+    //    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
