f78950dd41c038a483c8a533422ef78832a2f327
cockroach <weekend8394@gmail.com>
2019-03-27 17:00:53 +0800

    3/27 1.user

--- FootPrintIOS/SignInViewController.swift
+++ FootPrintIOS/SignInViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SignInViewController: UIViewController {
     let url_server = URL(string: common_url + "/AccountServlet")
+    var account:String? = ""
+    var password:String? = ""
     
     @IBOutlet weak var accountTextFild: UITextField!
     @IBOutlet weak var passwordTextFild: UITextField!
@@ -21,8 +23,8 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func clickLogin(_ sender: UIButton) {
-        let account = accountTextFild.text == nil ? "": accountTextFild.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-        let password = passwordTextFild.text == nil ? "" :passwordTextFild.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        account = accountTextFild.text == nil ? "": accountTextFild.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        password = passwordTextFild.text == nil ? "" :passwordTextFild.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         if account!.isEmpty || password!.isEmpty{
             let alert = UIAlertController(title: "note", message: "user name or password is invalid", preferredStyle: .alert)
             let action = UIAlertAction(title: "OK", style: .default)
@@ -37,25 +39,24 @@ class SignInViewController: UIViewController {
         requestParam["password"] = password
         
         executeTask(url_server!, requestParam as! [String : String]) { (data, response, error) in
+
             if error == nil {
                 if data != nil {
                     // 將輸入資料列印出來除錯用
                     print("input: \(String(data: data!, encoding: .utf8)!)")
                     
-                    if let result = try? JSONDecoder().decode( Bool.self, from: data!) {
+                     if String(data:data!,encoding: .utf8)! == "true" {
                          print("1234)")
-                        if result {
                             let saved = saveUser(user)
                             print("saved = \(saved)")
                                     let userDefaults = UserDefaults.standard
-                                    userDefaults.set(account, forKey: "account")
+                            userDefaults.set(self.account, forKey: "account")
                                     userDefaults.synchronize()
                             // 開啟下一頁
-                            
                             DispatchQueue.main.async {
                                 self.performSegue(withIdentifier: "next", sender: self)
                             }
-                        }
+                        
                     } else {
                         DispatchQueue.main.async {
                             let alert = UIAlertController(title: "note", message: "User name or password incorrect!", preferredStyle: .alert)
