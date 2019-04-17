6dbc140e48496c6864851a1e37766b3e199f2f7b
cockroach <weekend8394@gmail.com>
2019-04-17 10:04:23 +0800

    4/17 好友聊天

--- FootPrintIOS/RightChatTableViewCell.swift
+++ FootPrintIOS/RightChatTableViewCell.swift
@@ -11,5 +11,15 @@ import UIKit
 class RightChatTableViewCell: UITableViewCell {
     @IBOutlet weak var lb_RightMessage: UILabel!
     
+    @IBOutlet weak var chatimageview: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
     
 }
